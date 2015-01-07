e1b258aa5a4b5f8c205eea8be1395e8e4a861d98
Prashant Gahlot <gahlotpercy@gmail.com>
2015-01-07 18:09:12 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,9 @@ BOARD_HAS_NO_REAL_SDCARD := true
 BOARD_HAS_SDCARD_INTERNAL := true
 TARGET_RECOVERY_FSTAB := device/sony/nypon/config/fstab.st-ericsson
 
-
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_FORCE_KERNEL_ADDRESS  := 0x00008000
+TARGET_NO_SEPARATE_RECOVERY := true
 DEVICE_RESOLUTION :=540x960
 BOARD_CMDLINE := cachepolicy=writealloc noinitrd init=init board_id=1 root=/dev/ram0 rw rootwait console=ttyAMA2,115200n8 androidboot.console=ttyAMA2 androidboot.hardware=st-ericsson mem=96M@0 mem_mtrace=15M@96M mem_mshared=1M@111M mem_modem=16M@112M mem=32M@128M mem_issw=1M@160M hwmem=167M@161M mem=696M@328M vmalloc=384M mpcore_wdt.mpcore_margin=359
 
